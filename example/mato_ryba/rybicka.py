41bfffe4dfa62c502236400207c1c4c13fecb034
mato <matog@fks.sk>
2014-09-07 18:06:42 +0200

    blbosti

--- example/mato_ryba/rybicka.py
+++ example/mato_ryba/rybicka.py
@@ -2,8 +2,20 @@ from gaminator import *
 import math
 import random
 
+from bisect import bisect_left
+
+def binary_search(a, x, lo=0, hi=None):
+    hi = hi if hi is not None else len(a)
+    pos = bisect_left(a,x,lo,hi)
+    return (pos if pos != hi and a[pos] == x else -1)
+
 SIRKA = 800
 VYSKA = 600
+
+
+def randomIntDist(mi, ma, dist):
+    random.randInt(random.random()*dist[-1])
+    # TODO dorobit
 
 
 class Ryba(Vec):
