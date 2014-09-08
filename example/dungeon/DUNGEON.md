29699b336f64421d42e79028557fc693cca977f8
maaario <mlipovskysk@gmail.com>
2014-09-09 00:02:33 +0200

    Inventar, Pasca, Prisera, Zvitok

--- example/dungeon/DUNGEON.md
+++ example/dungeon/DUNGEON.md
@@ -4,28 +4,33 @@
  - Vec  - Stena, Item, Prisera, Pasce, Zamok, Dvere
 
 ## ONI:
-- pouzitie itemu 
-	- nastalaUdalost()
-- pasce 
+- pouzitie itemu `DONE`
+	- nastalaUdalost() - vseobecny efekt, potiony spustaju efekty
+- pasce `DONE`
 	- nacasujUdalost()
-- vypis statov 
+- vypis statov `DONE`
 	- Text()
-- inventar (bar, bez stackovania itemov) 
+- inventar (bar, bez stackovania itemov) `DONE`
+	- ked najdeme item presunieme do inventara, na inventari Q/E presun aktivneho
 	- list
-- otvaranie klucmi
-- kreslenie inventaru 
+- otvaranie klucmi `DONE`
+- kreslenie inventaru `DONE`
+- pauza / zvitok `DONE`
+	- novySvet()
 - vyhra levelu 	
 	- novy/nahradSvet()
-- save/load 			
+	- vytvorime noveho hraca, presunieme do noveho hraca Itemy/Zivoty
+- save/load 	
 	- praca so subormi
+	- staci medzi lvl-mi aby si nemuseli pamatat itemy na mape
 
 ## MY:
 - ovladanie hraca `DONE`
 - zrazky so stenami (v kroku) `DONE`
-- zrazky s priserami (reverse pohybu + efekt)
+- zrazky s priserami (reverse pohybu) `DONE`
 - uvodna miestnost + load levelu `DONE`
-- class prisera 
-- hlupa prisera
+- class prisera `DONE`
+- hlupa prisera `DONE`
 - class item `DONE`
 - jednodychy item `DONE`
 - obrazky vsetkeho (aby nemuseli nic kreslit)
--- example/dungeon/dungeon.py
+++ example/dungeon/dungeon.py
@@ -6,17 +6,11 @@ from gaminator import *
 
 #velkost dlazdiciek
 GRID = 30
+VYSKA_SPODNEHO_BARU = GRID + 16
 #Pomocne polia pre navigaciu
 DX = [-1,0,1,0]
 DY = [0,-1,0,1]
 KEY_POHYB = [pygame.K_a, pygame.K_w, pygame.K_d, pygame.K_s]
-
-##################################
-#   TOTO TU SOM ROBIL 3 HODINY   #
-##################################
-# HRAC NARAZA DO NEPRIECHODNYCH  #
-# VECI A ZAROVEN KLZE PO STENACH #
-##################################
 
 #self potrebuje mat x,y,rychlost,svet
 def daSaIst(self, smer4):
@@ -28,108 +22,331 @@ def daSaIst(self, smer4):
 		ss = (smer8+okolie+8)%8
 		ny = (self.y + DDY[ss]*(GRID/2-1) + DDY[smer8]*self.rychlost) / GRID
 		nx = (self.x + DDX[ss]*(GRID/2-1) + DDX[smer8]*self.rychlost) / GRID
-		ny = min( max(0, ny), self.svet.vyska-1)
-		nx = min( max(0, nx), self.svet.sirka-1) 
-		if (self.svet.mapa[ny][nx]!=0) and (not self.svet.mapa[ny][nx].priechodne): return False
+		nny = min( max(0, ny), self.svet.vyska-1)
+		nnx = min( max(0, nx), self.svet.sirka-1) 
+		if (self.svet.mapa[nny][nnx]!=0) or (nnx != nx) or (nny != ny): return False
 	return True
 	
-# Objekty, ktore su pouzivane sami o sebe
-	
+# Staticke objekty
+class Stena(Obrazok):
+	def nastav(self, x, y, znak):
+		self.x = x
+		self.y = y
+		self.nastavSubor("grafika", "stena2.png")
+
+class Dvere(Obrazok):
+	def nastav(self, x, y, znak):
+		self.x = x
+		self.y = y
+		self.typ = ord(znak) - ord('A')
+		self.nastavSubor("grafika","drevozamok"+str(self.typ) +".png")
+
+class Pasca(Obrazok):								#ma ovladat iba seba?
+	def nastav(self, x, y, znak):
+		self.x = x
+		self.y = y
+		self.vysunuta = False
+		self.svet.nastalaUdalost("VYSUN",self)
+
+	@priUdalosti("VYSUN")
+	def vysun(self,pasca):
+		if self != pasca: return
+		self.nastavSubor("grafika","ohen.png")
+		self.vysunuta = True
+		self.svet.nacasujUdalost(1300, "ZASUN",self)
+
+	@priUdalosti("ZASUN")
+	def zasun(self,pasca):
+		if self != pasca: return
+		self.nastavSubor("grafika","ohenzamrezami.png")
+		self.vysunuta = False
+		self.svet.nacasujUdalost(1300, "VYSUN",self)
+
+# Specialne template objekty
+class Lifebar(Vec):
+	def nastav(self, prisera):
+		self.prisera = prisera
+		self.z = 100
+
+	def nakresli(self, kreslic):
+		self.x = self.prisera.x
+		self.y = self.prisera.y
+		z = self.prisera.zivoty
+		m = self.prisera.maxZivoty
+		if   1.*z/m > 0.5:  kreslic.farba = Farba.ZELENA
+		elif 1.*z/m > 0.25: kreslic.farba = Farba.ZLTA
+		else:	            kreslic.farba = Farba.CERVENA
+		kreslic.obdlznik( (-GRID/2,-GRID/2-5), GRID*z/m, 3)
+
+class Prisera(Obrazok):
+	def nastav(self, x, y, znak):
+		self.x = x
+		self.y = y
+		self.nazov = str(self.__class__)[17:-2]
+		
+		self.stoj = False
+		self.rychlost = 0
+		self.smer = 0
+
+		self.lifebar = Lifebar(self.svet, self)
+		self.zivoty = 0
+		self.maxZivoty = 0
+		self.utok = 0
+		self.odmeny = [] #zoznam charakterovych skratiek pre itemy
+
+	def odstupPoZrazke(self):
+		if daSaIst(self,(self.smer+2)%4 ):
+			self.x -= self.rychlost*DX[self.smer]
+			self.y -= self.rychlost*DY[self.smer]
+
+	def zomri(self):
+		for odmena in self.odmeny:
+			if odmena in LEGENDA:
+				LEGENDA[odmena](self.svet, self.x, self.y, odmena)
+		self.lifebar.znic()
+		self.znic()
+
+	@priUdalosti("ZRANENIE")
+	def zranenenie(self, mnozstvo, prisera=None):
+		if prisera == None or prisera == self:
+			self.zivoty -= mnozstvo
+			if self.zivoty <= 0:
+				self.zomri()
+
+	@priUdalosti("ZMRAZ")
+	def zmraz(self,cas):
+		self.svet.nacasujUdalost(cas,"ODMRAZ")
+		self.stoj = True
+
+	@priUdalosti("ODMRAZ")
+	def odmraz(self):
+		self.stoj = False
+
+	@priZrazke(Dvere)
+	def dvereVCeste(self,dvere):
+		self.odstupPoZrazke()
+
+class Item(Obrazok):
+	def nastav(self, x, y, znak):
+		self.x = x
+		self.y = y
+		self.nazov = str(self.__class__)[17:-2]
+		
+	def pouzi(self):
+		pass
+
+# GUI
+class StatusBar(Vec):
+	def nastav(self):
+		self.x = 0
+		self.y = okno.vyska-GRID-16
+		self.zivoty = Text(self.svet)
+		self.zivoty.y = okno.vyska - GRID - 8
+		self.zivoty.x = okno.sirka / 2
+		self.zivoty.z = 100
+		self.zivoty.aktualizuj(velkost = 20)
+
+	def nakresli(self, kreslic):
+		z = self.svet.hrac.zivoty
+		m = self.svet.hrac.maxZivoty
+		if   1.*z/m > 0.5:  kreslic.farba = Farba.ZELENA
+		elif 1.*z/m > 0.25: kreslic.farba = Farba.ZLTA
+		else:	            kreslic.farba = Farba.CERVENA
+		kreslic.obdlznik( (0,0), okno.sirka*z/m, 16)
+		self.zivoty.aktualizuj( str(z)+"/"+str(m) )
+
+class Inventar(Vec):
+	def nastav(self, velkost):
+		self.predmety = []
+		self.velkost = velkost
+		self.x = (okno.sirka-velkost*GRID)/2
+		self.y = okno.vyska - GRID
+		self.z = 100
+		self.oznaceny = 0
+
+	def nakresli(self,kreslic):
+		kreslic.farba = Farba(100, 50, 0)
+		for i in range(self.velkost):
+			kreslic.obdlznik( (i*GRID, 0), GRID, GRID,2)
+		kreslic.farba = Farba.CERVENA
+		kreslic.obdlznik( (self.oznaceny*GRID, 0), GRID, GRID,2)
+
+	def pridajPredmet(self, predmet):
+		if len(self.predmety) >= self.velkost:
+			return
+		else:
+			self.predmety.append(predmet)
+			predmet.x = self.x+len(self.predmety)*GRID - GRID/2
+			predmet.y = okno.vyska - GRID/2
+
+	def pouziPredmet(self, index):
+		if self.predmety[index] == None: return
+		self.predmety[index].pouzi()
+		for i in range(index, len(self.predmety)-1):
+			self.predmety[i] = self.predmety[i+1]
+			self.predmety[i].x -= GRID
+		self.predmety.pop()
+
+	@priUdalosti("KLAVES DOLE")
+	def klaves(self, klaves, unicode):
+		if klaves == pygame.K_q:
+			self.oznaceny = (self.oznaceny - 1 + self.velkost) % self.velkost
+		elif klaves == pygame.K_e:
+			self.oznaceny = (self.oznaceny + 1 + self.velkost) % self.velkost
+		elif klaves == pygame.K_SPACE:
+			if not isinstance( self.predmety[self.oznaceny], Kluc):
+				self.pouziPredmet(self.oznaceny)
+
+# Hrac
 class Hrac(Obrazok):
-	def nastav(self, x, y, *ine):
+	def nastav(self, x, y, znak):
 		self.x = x
 		self.y = y
 		self.nastavSubor("grafika", "hrac.png")
-		self.priechodne = False
 
+		self.inventar = None
+		
+		self.zivoty = 300
+		self.maxZivoty = 300
+		self.utok = 1 
 		self.rychlost = 2
-		self.zivoty = 100
 
 	def krok(self):
-		#pohyb, ktory overuje priechodne a nepriechodne objekty
+		#pohyb, ktory overuje zrazky so stenami
 		for i in range(4):
 			if self.svet.stlacene[KEY_POHYB[i]] and daSaIst(self, i):
 				self.x += self.rychlost*DX[i]
 				self.y += self.rychlost*DY[i]
 
-class Stena(Obrazok):
-	def nastav(self, x, y, *ine):
-		self.x = x
-		self.y = y
-		self.priechodne = False
-		self.nastavSubor("grafika", "stena2.png")
-
-class Dvere(Obrazok):
-	def nastav(self, x, y, *ine):
-		self.x = x
-		self.y = y
-		self.priechodne = False
-		self.typ = ord(ine[0]) - ord('A')
-		self.nastavSubor("grafika","drevozamok"+str(self.typ) +".png")
-
-	@priZrazke(Hrac)
-	def pokusOOtvorenie(self,hrac):
-		pass
-
-# Classy, ktore su zakladom pre rozne typy priser/itemov
-
-class Prisera(Vec):
-	def nastav(self, x, y, *ine):
-		self.x = x
-		self.y = y
-		self.priechodne = False
-
-		self.zivoty = 0
-		self.utok = 0
-		self.odmeny = []
+	def zranenenie(self, mnozstvo):
+		self.zivoty -= mnozstvo
+		if self.zivoty <= 0:
+			self.zomri()
 
 	def zomri(self):
-		self.znic()
+		hra.koniec()
 
-class Item(Obrazok):
-	def nastav(self, x, y, *ine):
-		self.x = x
-		self.y = y
-		self.priechodne = True
-		self.hrac = None
-		
-	@priZrazke(Hrac)
-	def zodvihnuteHracom(self, hrac):
-		pass
+	@priUdalosti("VYLIEC HRACA")
+	def vyliec(self, mnozstvo):
+		self.zivoty = min( self.zivoty + mnozstvo, self.maxZivoty )
 
-	def pouzi(self):
-		pass
+	def odstupPoZrazke(self):
+		for i in range(4):
+			if self.svet.stlacene[KEY_POHYB[i]] and daSaIst(self,(i+2)%4 ):
+				self.x -= self.rychlost*DX[i]
+				self.y -= self.rychlost*DY[i]
+
+	@priZrazke(Dvere)
+	def pokusOOtvorenie(self, dvere):
+		for i in range( len(self.inventar.predmety) ):
+			if self.inventar.predmety[i].nazov == "Kluc" and (self.inventar.predmety[i].typ == dvere.typ):
+				self.inventar.pouziPredmet(i)
+				dvere.znic()
+				break
+		self.odstupPoZrazke()
+	
+	@priZrazke(Pasca)
+	def padDoPasce(self, pasca):
+		if pasca.vysunuta:
+			self.zranenenie(3)
+
+	@priZrazke(Prisera)
+	def bojSPriserou(self, prisera):
+		self.svet.nastalaUdalost("ZRANENIE",self.utok,prisera)
+		self.zranenenie(prisera.utok)
+		self.odstupPoZrazke()
+		self.odstupPoZrazke()
+
+
+	@priZrazke(Item)
+	def zoberItem(self, predmet):
+		if predmet.nazov == "Zvitok":
+			self.odstupPoZrazke()
+		else:
+			self.inventar.pridajPredmet(predmet)
 
 # Odvodeniny od zakladnych class
-
-class NapojZivoty(Item, Obrazok):
-	def nastav(self, x, y, *ine):
-		super(NapojZivoty, self).nastav(x,y)
+class NapojZivoty(Item):
+	def nastav(self, x, y, znak):
+		super(NapojZivoty, self).nastav(x,y,znak)
 		self.nastavSubor("grafika","napojZivoty.png")
 
-	def zodvihnuteHracom(self, hrac):
-		self.hrac = hrac	
-
 	def pouzi(self):
-		self.hrac.zivoty += 30
+		self.svet.nastalaUdalost("VYLIEC HRACA", 50)
+		self.znic()
 
-class Kluc(Item, Obrazok):
-	def nastav(self, x, y, *ine):
-		super(Kluc, self).nastav(x,y)
-		self.typ = int(ine[0])
+class Kluc(Item):
+	def nastav(self, x, y, znak):
+		super(Kluc, self).nastav(x,y,znak)
+		self.typ = int(znak)
 		self.nastavSubor("grafika","kluc"+str(self.typ)+".png")
 
-# Svety
+	def pouzi(self):
+		self.znic()
 
-class Miestnost(Svet):
-	legenda = {
+class Zmrazovac(Item):
+	def nastav(self, x, y, znak):
+		super(Zmrazovac, self).nastav(x,y,znak)
+		self.nastavSubor("grafika","zmrazovac.png")
+
+	def pouzi(self):
+		self.svet.nastalaUdalost("ZMRAZ", 3000)
+		self.znic()
+
+class Zvitok(Item):
+	def nastav(self, x, y, znak):
+		super(Zvitok, self).nastav(x,y,znak)
+		self.nastavSubor("grafika","zvitok.png")
+		self.text = open("zvitok"+str(znak)+".txt","r").read().strip().split("\n")
+
+	@priZrazke(Hrac)
+	def precitaj(self, hrac):
+		hra.otvorSvet(SvetZvitok(self.text))
+
+class Duch(Prisera):
+	def nastav(self, x, y, znak):
+		super(Duch, self).nastav(x,y,znak)
+		self.nastavSubor("grafika", "duchdraka.png")
+		self.rychlost = 1
+		self.zivoty = self.maxZivoty = 32
+		self.utok = 3
+		odmena = 'ZL  '
+		self.odmeny = [odmena[nahodneCislo(0,3)]]
+
+
+	def krok(self):
+		if self.stoj:
+			return
+		for i in range(4):
+			if not daSaIst(self, self.smer): 
+				self.smer = (self.smer + 1) % 4
+			else:
+				break
+
+		if nahodneCislo(0, 120) == 0:
+			self.smer = nahodneCislo(0, 3)
+
+		if daSaIst(self, self.smer):		
+			self.x += DX[self.smer]*self.rychlost
+			self.y += DY[self.smer]*self.rychlost
+
+# Svety
+LEGENDA = {
 		'H': Hrac,
+		#staticke
 		'#': Stena,
-		'Z': NapojZivoty,
-		'0': Kluc,  '1': Kluc,  '2': Kluc,  '3': Kluc,	#typy kluca
-		'A': Dvere, 'B': Dvere, 'C': Dvere, 'D': Dvere
+		'P': Pasca,
+		'A': Dvere, 'B': Dvere, 'C': Dvere, 'D': Dvere, #typy dveri 0-3
+		#itemy
+		'L': NapojZivoty,
+		'Z': Zmrazovac,
+		'0': Kluc,  '1': Kluc,  '2': Kluc,  '3': Kluc,	#typy kluca 0-3
+		'a': Zvitok,
+		#prisery
+		'@': Duch
 	}
 
+class Miestnost(Svet):
 	def nastav(self):
 		# Dvojrozmerne pole objektov, jediny objekt, co nie je na mape je hrac
 		self.mapa = []
@@ -143,11 +360,62 @@ class Miestnost(Svet):
 		for i in range(self.vyska):
 			for j in range(self.sirka):
 				if mapa[i][j] == 'H':
-					self.hrac = Hrac(self, j*GRID+GRID/2, i*GRID+GRID/2) 
-				elif mapa[i][j] in self.legenda : 
-					self.mapa[i][j] = self.legenda[mapa[i][j]](self, j*GRID+GRID/2, i*GRID+GRID/2, mapa[i][j])
+					self.hrac = Hrac(self, j*GRID+GRID/2, i*GRID+GRID/2,mapa[i][j]) 
+				elif mapa[i][j] in LEGENDA : 
+					LEGENDA[mapa[i][j]](self, j*GRID+GRID/2, i*GRID+GRID/2, mapa[i][j])
+					if mapa[i][j] == '#': self.mapa[i][j] = 1
 		okno.sirka = self.sirka*GRID
-		okno.vyska = self.vyska*GRID
+		okno.vyska = self.vyska*GRID + VYSKA_SPODNEHO_BARU
+		self.hrac.inventar = Inventar(self,10)
+		StatusBar(self)
+	
+	@priUdalosti("KLAVES DOLE")
+	def klaves(self, klaves, unicode):
+		if klaves == pygame.K_p:
+			hra.otvorSvet(Pauza())
+
+class Pauza(Svet):
+	def nastav(self):
+		t1 = Text(self)
+		t1.x = okno.sirka/2
+		t1.y = okno.vyska/2
+		t1.aktualizuj(velkost = 30, text = "PAUZA")
+
+		t2 = Text(self)
+		t2.x = okno.sirka/2
+		t2.y = okno.vyska/2+20
+		t2.aktualizuj(velkost = 20, text = "Stlacte [P] pre pokracovanie.")
+
+	@priUdalosti("KLAVES DOLE")
+	def klaves(self, klaves, unicode):
+		if klaves == pygame.K_p:
+			hra.zatvorSvet()
+
+class SvetZvitok(Svet):
+	def nastav(self, text):
+		okrajHore = 30
+		okrajVlavo = 30
+		velkost = 20
+
+		for line in text:
+			t = Text(self)
+			t.x = okrajVlavo
+			t.y = okrajHore
+			okrajHore += 20
+			t.zarovnajX = 0
+			t.aktualizuj(text = line, velkost = velkost)
+
+		t = Text(self)
+		t.x = 0
+		t.y = okno.vyska
+		t.zarovnajX = 0
+		t.zarovnajY = 1
+		t.aktualizuj(text = "Pre navrat do hry stlacte [ESC].", velkost = velkost)
+
+	@priUdalosti("KLAVES DOLE")
+	def odid(self, klaves, unicode):
+		if klaves == pygame.K_ESCAPE:
+			hra.zatvorSvet()
 
 okno.nazov = "Dungeon"
 hra.start(Miestnost())
